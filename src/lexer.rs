52b5f2495fdee6877605fdb48c301508b727c215
Filip Andrei <filandrei4@gmail.com>
2025-12-14 01:44:31 +0200

    feat: integer parsing

--- src/lexer.rs
+++ src/lexer.rs
@@ -31,18 +31,17 @@ pub enum LexiToken {
     Int32,
 
     // End of file
-    Eof
+    Eof,
 }
 
-
-pub struct Lexer{
+pub struct Lexer {
     chars: Vec<char>,
     cursor: usize,
     word_start: usize,
     size: usize,
 }
 
-impl Lexer  {
+impl Lexer {
     pub fn new(input_buff: &str) -> Self {
         Self {
             chars: input_buff.chars().collect(),
@@ -57,7 +56,9 @@ impl Lexer  {
     fn skip_whitespace(&mut self) {
         loop {
             match self.chars[self.cursor] {
-                ' ' | '\t' | '\n' => { self.cursor += 1;} ,
+                ' ' | '\t' | '\n' => {
+                    self.cursor += 1;
+                }
                 _ => {
                     break;
                 }
@@ -68,16 +69,33 @@ impl Lexer  {
     /// Returns the Integer literal parsed from [word_start..cursor)
     /// If word_start == cursor, returns a token formed from the character found at the cursor
     fn parse_lit_int(&mut self) -> Option<LexiToken> {
-        todo!("Not implemented yet");
+        if self.word_start == self.cursor {
+            let res: i32 = self.chars[self.cursor] as i32;
+            if res < 10 {
+                return Some(LexiToken::LitInt(res));
+            } else {
+                return None;
+            }
+        }
+        let slice: String = self.chars[self.word_start..self.cursor].iter().collect();
+
+        match slice.parse::<i32>() {
+            Ok(num) => {
+                return Some(LexiToken::LitInt(num));
+            }
+            Err(_) => {
+                return None;
+            }
+        }
     }
 
-    /// Returns the Float literal parsed from [word_start..cursor], with floating dot point in
+    /// Returns the Float literal parse from [word_start..cursor], with floating dot point in
     /// point_pos position, relative to the string to be parsed
     fn parse_lit_float(&mut self, point_pos: usize) -> Option<LexiToken> {
         todo!("Not implemented yet");
     }
 
-    /// Parses the numerical literal token found at the cursor 
+    /// Parses the numerical literal token found at the cursor
     /// The cursor is left at the start of the next unprocessed char
     /// # Returns
     /// Some(Eof) if the end of file was reached
@@ -85,15 +103,25 @@ impl Lexer  {
     /// Some(LitInt) if the literal could be interpreted as an integer literal (no floating point)
     /// Some(LitFloat) if the literal could be interpreted as a float integer (one . found inside
     /// the literal)
-    fn parse_num_lit(&mut self) -> Option<LexiToken>{
+    fn parse_num_lit(&mut self) -> Option<LexiToken> {
         self.word_start = self.cursor;
         let mut point_pos: i64 = -1; // -1 means no floating point
         loop {
-            if self.cursor + 1 >= self.size {
-                if self.cursor == self.word_start { // No token was read
+            if self.cursor >= self.size {
+                println!("Am atins eof");
+                println!(
+                    "start: {}, cursor: {}, size: {}",
+                    self.word_start, self.cursor, self.size
+                );
+                if self.cursor == self.word_start {
+                    // No token was read
                     return Some(LexiToken::Eof); // End of file reached,
                 }
-                if point_pos == -1 { return self.parse_lit_int(); } else { return self.parse_lit_float(point_pos as usize); }
+                if point_pos == -1 {
+                    return self.parse_lit_int();
+                } else {
+                    return self.parse_lit_float(point_pos as usize);
+                }
             }
 
             if self.chars[self.cursor] == '.' {
@@ -101,10 +129,16 @@ impl Lexer  {
                     // Second . found - Return None
                     return None;
                 }
-               point_pos = (self.cursor - self.word_start) as i64;
+                point_pos = (self.cursor - self.word_start) as i64;
             } else if !self.chars[self.cursor].is_digit(10) {
-                if self.cursor == self.word_start { return None; }
-                if point_pos == -1 { return self.parse_lit_int(); } else { return self.parse_lit_float(point_pos as usize); }
+                if self.cursor == self.word_start {
+                    return None;
+                }
+                if point_pos == -1 {
+                    return self.parse_lit_int();
+                } else {
+                    return self.parse_lit_float(point_pos as usize);
+                }
             }
             self.cursor += 1;
         }
@@ -127,7 +161,7 @@ impl Lexer  {
             'a'..'z' | 'A'..'Z' | '_' => self.parse_alphastr_lit(),
             '0'..'9' => self.parse_num_lit(),
 
-            _ => None
+            _ => None,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod lexer;
 use crate::lexer::Lexer;
 
 fn main() {
-    let source_code = "1234";
+    let source_code = "123.4";
     let mut lexer: Lexer = Lexer::new(&source_code);
     println!("{:?}", lexer.next_token());
 }
